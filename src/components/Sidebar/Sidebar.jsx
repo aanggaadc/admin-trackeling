33743a1ec3039543e666a4d7a4c142f8ecfe2a36
kisbayu <kisbayu.k.a@gmail.com>
2022-05-17 14:47:57 +0700

    add sidebar styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
-import "./Sidebar.css"
-import Image from 'react-bootstrap/Image'
-import { Link, useNavigate } from "react-router-dom"
+import "./Sidebar.scss"
+import { BiTrip,BiUser,BiAddToQueue } from "react-icons/bi";
+import Logo from '../../trackling.png'
+// import { Link, useNavigate } from "react-router-dom"
 
 function Sidebar() {
   return (
     <div className="sidebar">
         <div className="top">
             <div className="logo">
-                <Image src="../../trackling.png" alt='LOGO'/>
+                <img src={Logo} alt="TRACKELING"/>
             </div>
+        </div>
+        <div className="subtitle">
+            Dashboard
         </div>
         <div className="center">
             <ul>
-                <li>Trip List</li>
-                <li>Create Trip</li>
-                <li>User List</li>
+                <li>
+                    <BiTrip className='icon'/>
+                    <span>Trip List</span>
+                </li>
+                <li>
+                    <BiAddToQueue className='icon'/>
+                    <span>Create Trip</span>
+                </li>
+                <li>
+                    <BiUser className='icon'/>
+                    <span>User List</span>
+                </li>
             </ul>
         </div>
     </div>
--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import './List.scss'
 import Sidebar from '../../components/Sidebar/Sidebar'
 
 function List() {
   return (
     <div className="list">
         <Sidebar />
+        <div className="listContainer">
+          container
+        </div>
     </div>
   )
 }
