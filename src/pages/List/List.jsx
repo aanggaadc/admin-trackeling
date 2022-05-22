e773655d24c4820e0b84aece8f9ef2532d0a0db1
kisbayu <kisbayu.k.a@gmail.com>
2022-05-22 17:22:14 +0700

    add chart component

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './List.scss'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import Navbar from '../../components/Navbar/Navbar'
+import Chart from '../../components/Chart/Chart'
 
 function List() {
   return (
@@ -9,6 +10,9 @@ function List() {
         <Sidebar />
         <div className="listContainer">
           <Navbar />
+          <div className="charts">
+          <Chart aspect={4 / 1} title="Last 6 Month" />
+        </div>
         </div>
     </div>
   )
