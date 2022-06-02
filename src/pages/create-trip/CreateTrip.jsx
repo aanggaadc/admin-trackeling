8b5b1d78ca8c5352942c93545940f940540aa48f
RizkyPDA <muhammad.syadrie11@gmail.com>
2022-06-03 01:12:37 +0700

    tambah field form

--- src/pages/create-trip/CreateTrip.jsx
+++ src/pages/create-trip/CreateTrip.jsx
@@ -2,17 +2,36 @@ import React from "react";
 import "./createTrip.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import { Formik, Form } from "formik";
 
 function createTrip() {
   return (
     <div>
-      <Sidebar />
-      <div className="createTrip"></div>
-      <div className="createTripContainer">
-        <Navbar />
-        <form action="">
-          <input type="text" />
-        </form>
+      <div className="createTrip">
+        <Sidebar />
+        <div className="createTripContainer">
+          <Navbar />
+          <div className="formInput">
+            <Formik
+              initialValues={{
+                destination: "",
+                description: "",
+                uploadImage: "",
+              }}
+            >
+              <Form id="createTripForm">
+                <div className="formGroup">
+                  <label for="destination">Destination</label>
+                  <input type="text" className="formControl" name="destination" id="destination"></input>
+                  <label for="description">Description</label>
+                  <input type="text" className="formControl" name="description" id="description"></input>
+                  <label for="uploadImage">Upload Image</label>
+                  <input type="text" className="formControl" name="uploadImage" id="uploadImage"></input>
+                </div>
+              </Form>
+            </Formik>
+          </div>
+        </div>
       </div>
     </div>
   );
