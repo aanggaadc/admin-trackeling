a039d1e8371a21c993c251a9cd29268f3dc01f88
RizkyPDA <muhammad.syadrie11@gmail.com>
2022-06-03 23:32:41 +0700

    upload image

--- src/pages/create-trip/CreateTrip.jsx
+++ src/pages/create-trip/CreateTrip.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import { React, useState } from "react";
 import "./createTrip.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { Formik, Form } from "formik";
 
-function createTrip() {
+function CreateTrip() {
+  const [file, setFile] = useState("");
+
   return (
     <div>
       <div className="createTrip">
@@ -16,20 +18,67 @@ function createTrip() {
               initialValues={{
                 destination: "",
                 description: "",
-                uploadImage: "",
+                file: "",
+              }}
+              onSubmit={(values) => {
+                const formData = new FormData();
+                formData.append("destination", values.destination);
+                formData.append("description", values.description);
+                formData.append("file", values.file);
+
+                console.log(values);
               }}
             >
-              <Form id="createTripForm">
-                <div className="formGroup">
-                  <label for="destination">Destination</label>
-                  <input type="text" className="formControl" name="destination" id="destination"></input>
-                  <label for="description">Description</label>
-                  <input type="text" className="formControl" name="description" id="description"></input>
-                  <label for="uploadImage">Upload Image</label>
-                  <input type="text" className="formControl" name="uploadImage" id="uploadImage"></input>
-                </div>
-              </Form>
+              {({ handleSubmit, handleChange, setFieldValue }) => (
+                <Form id="createTripForm">
+                  <div className="formGroup">
+                    <label for="destination">Destination</label>
+                    <input
+                      type="text"
+                      onChange={(e) => {
+                        setFieldValue("destination", e.target);
+                      }}
+                      className="formControl"
+                      name="destination"
+                      id="destination"
+                    ></input>
+                    <label for="description">Description</label>
+                    <input
+                      type="text"
+                      onChange={(e) => {
+                        setFieldValue("description", e.target);
+                      }}
+                      className="formControl"
+                      name="description"
+                      id="description"
+                    ></input>
+                    <label for="uploadImage">Upload Image</label>
+
+                    <input
+                      type="file"
+                      accept="image/*"
+                      name="file"
+                      onChange={(e) => {
+                        setFile(e.target.files[0]);
+                        setFieldValue("file", e.currentTarget.files[0]);
+                      }}
+                    />
+                    <div className="imgContainer">
+                      <img className="imgUpload img-fluid" src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"} />
+                    </div>
+                  </div>
+                  <div className="btn">
+                    <button type="submit" onClick={handleSubmit} className="btnSubmit">
+                      Submit
+                    </button>
+                    <button className="btnBack" type="button">
+                      Back
+                    </button>
+                  </div>
+                </Form>
+              )}
             </Formik>
+            <div></div>
           </div>
         </div>
       </div>
@@ -37,4 +86,4 @@ function createTrip() {
   );
 }
 
-export default createTrip;
+export default CreateTrip;
