6a5035171e6898ba82b921bf7b4d82070d5b1006
deriscode <jdrepositories@gmail.com>
2022-06-08 23:47:59 +0700

    selesai membuat tabel

---
+++ src/components/table_users/TableUsers.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+
+function TableUsers() {
+	return (
+		<Table striped bordered responsive>
+			<thead>
+				<tr>
+					<th>#</th>
+					{Array.from({ length: 9 }).map((_, index) => (
+						<th key={index}>Table heading</th>
+					))}
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					{Array.from({ length: 9 }).map((_, index) => (
+						<td key={index}>Table cell {index}</td>
+					))}
+				</tr>
+				<tr>
+					<td>2</td>
+					{Array.from({ length: 9 }).map((_, index) => (
+						<td key={index}>Table cell {index}</td>
+					))}
+				</tr>
+				<tr>
+					<td>3</td>
+					{Array.from({ length: 9 }).map((_, index) => (
+						<td key={index}>Table cell {index}</td>
+					))}
+				</tr>
+			</tbody>
+		</Table>
+	);
+}
+
+export default TableUsers;
