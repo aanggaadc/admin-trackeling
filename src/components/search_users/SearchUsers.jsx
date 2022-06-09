3019153503c059fb686c3aebe3a4e9d71ab0cbbc
RizkyPDA <muhammad.syadrie11@gmail.com>
2022-06-09 14:16:57 +0700

    create static filter for users

--- src/components/search_users/SearchUsers.jsx
+++ src/components/search_users/SearchUsers.jsx
@@ -1,7 +1,68 @@
 import React from "react";
+import { Card, Button, Form } from "react-bootstrap";
 
-function SearchUsers() {
-	return <div>SearchUsers</div>;
+function FilterContent({ setFilter, filter }) {
+  return (
+    <div className="mt-3">
+      <Card>
+        <Card.Header className="">Search Users</Card.Header>
+        <Card.Body>
+          <Form>
+            <div className="row">
+              <div className="col-md-6">
+                <Form.Group className="mb-3" controlId="formBasicUsername">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Search By Username"
+                    onChange={(e) => {
+                      setFilter({ ...filter, name: e.target.value });
+                    }}
+                  />
+                </Form.Group>
+              </div>
+              <div className="col-md-6">
+                <Form.Group className="mb-3" controlId="formBasicTripName">
+                  <Form.Label>Trip Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Search By Trip Name"
+                    onChange={(e) => {
+                      setFilter({ ...filter, price: e.target.value });
+                    }}
+                  />
+                </Form.Group>
+              </div>
+              <div className="col-md-6">
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Gender</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Search By Gender"
+                    onChange={(e) => {
+                      setFilter({ ...filter, price: e.target.value });
+                    }}
+                  />
+                </Form.Group>
+              </div>
+              <div className="col-md-6">
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Location</Form.Label>
+                  <Form.Control
+                    type="Number"
+                    placeholder="Search By User Location"
+                    onChange={(e) => {
+                      setFilter({ ...filter, price: e.target.value });
+                    }}
+                  />
+                </Form.Group>
+              </div>
+            </div>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
-export default SearchUsers;
+export default FilterContent;
