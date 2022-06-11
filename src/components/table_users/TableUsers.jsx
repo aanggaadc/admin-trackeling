e125d7ab0ad9856be85dd6b50963877f89cedc86
deriscode <jdrepositories@gmail.com>
2022-06-11 15:44:50 +0700

    revisi tabel menjadi bootstrap table next

--- src/components/table_users/TableUsers.jsx
+++ src/components/table_users/TableUsers.jsx
@@ -1,234 +1,90 @@
 import React, { useState } from "react";
-import { Table } from "react-bootstrap";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
-import { MDBDataTableV5 } from "mdbreact";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
 import Axios from "axios";
+import "./TableUsers.scss";
 
 function TableUsers() {
-	const [datatable, setDatatable] = useState({
-		columns: [
-			{
-				label: "Username",
-				field: "username",
-				width: 270,
-				// attributes: {
-				// 	"aria-controls": "DataTable",
-				// 	"aria-label": "Username",
-				// },
+	const columns = [
+		{
+			dataField: "username",
+			text: "Username",
+			headerStyle: () => {
+				return { width: "35%" };
 			},
-			{
-				label: "Email",
-				field: "email",
-				width: 270,
+		},
+		{
+			dataField: "email",
+			text: "Email",
+			headerStyle: () => {
+				return { width: "40%" };
 			},
-			{
-				label: "Gender",
-				field: "gender",
-				sort: "disabled",
-				width: 150,
+		},
+		{
+			dataField: "age",
+			text: "Age",
+			headerStyle: () => {
+				return { width: "20%" };
 			},
-			{
-				label: "Age",
-				field: "age",
-				sort: "asc",
-				width: 100,
+		},
+		{
+			dataField: "gender",
+			text: "Gender",
+			headerStyle: () => {
+				return { width: "30%" };
 			},
-			{
-				label: "Location",
-				field: "location",
-				width: 150,
+		},
+		{
+			dataField: "location",
+			text: "Location",
+			headerStyle: () => {
+				return { width: "30%" };
 			},
-			{
-				label: "Phone Number",
-				field: "phone_number",
-				width: 100,
+		},
+		{
+			dataField: "phoneNumber",
+			text: "Phone Number",
+			headerStyle: () => {
+				return { width: "35%" };
 			},
-		],
-		rows: [
-			{
-				username: "Tiger Nixon",
-				email: "System Architect",
-				gender: "Edinburgh",
-				age: "61",
-				location: "2011/04/25",
-				phone_number: "$320",
-			},
-			{
-				username: "Garrett Winters",
-				email: "Accountant",
-				gender: "Tokyo",
-				age: "63",
-				location: "2011/07/25",
-				phone_number: "$170",
-			},
-			{
-				username: "Ashton Cox",
-				email: "Junior Technical Author",
-				gender: "San Francisco",
-				age: "66",
-				location: "2009/01/12",
-				phone_number: "$86",
-			},
-			{
-				username: "Cedric Kelly",
-				email: "Senior Javascript Developer",
-				gender: "Edinburgh",
-				age: "22",
-				location: "2012/03/29",
-				phone_number: "$433",
-			},
-			{
-				username: "Airi Satou",
-				email: "Accountant",
-				gender: "Tokyo",
-				age: "33",
-				location: "2008/11/28",
-				phone_number: "$162",
-			},
-			{
-				username: "Brielle Williamson",
-				email: "Integration Specialist",
-				gender: "New York",
-				age: "61",
-				location: "2012/12/02",
-				phone_number: "$372",
-			},
-			{
-				username: "Herrod Chandler",
-				email: "Sales Assistant",
-				gender: "San Francisco",
-				age: "59",
-				location: "2012/08/06",
-				phone_number: "$137",
-			},
-			{
-				username: "Rhona Davidson",
-				email: "Integration Specialist",
-				gender: "Tokyo",
-				age: "55",
-				location: "2010/10/14",
-				phone_number: "$327",
-			},
-			{
-				username: "Colleen Hurst",
-				email: "Javascript Developer",
-				gender: "San Francisco",
-				age: "39",
-				location: "2009/09/15",
-				phone_number: "$205",
-			},
-			{
-				username: "Sonya Frost",
-				email: "Software Engineer",
-				gender: "Edinburgh",
-				age: "23",
-				location: "2008/12/13",
-				phone_number: "$103",
-			},
-			{
-				username: "Ashton Cox",
-				email: "Junior Technical Author",
-				gender: "San Francisco",
-				age: "66",
-				location: "2009/01/12",
-				phone_number: "$86",
-			},
-			{
-				username: "Cedric Kelly",
-				email: "Senior Javascript Developer",
-				gender: "Edinburgh",
-				age: "22",
-				location: "2012/03/29",
-				phone_number: "$433",
-			},
-			{
-				username: "Airi Satou",
-				email: "Accountant",
-				gender: "Tokyo",
-				age: "33",
-				location: "2008/11/28",
-				phone_number: "$162",
-			},
-			{
-				username: "Brielle Williamson",
-				email: "Integration Specialist",
-				gender: "New York",
-				age: "61",
-				location: "2012/12/02",
-				phone_number: "$372",
-			},
-			{
-				username: "Herrod Chandler",
-				email: "Sales Assistant",
-				gender: "San Francisco",
-				age: "59",
-				location: "2012/08/06",
-				phone_number: "$137",
-			},
-			{
-				username: "Rhona Davidson",
-				email: "Integration Specialist",
-				gender: "Tokyo",
-				age: "55",
-				location: "2010/10/14",
-				phone_number: "$327",
-			},
-			{
-				username: "Colleen Hurst",
-				email: "Javascript Developer",
-				gender: "San Francisco",
-				age: "39",
-				location: "2009/09/15",
-				phone_number: "$205",
-			},
-			{
-				username: "Sonya Frost",
-				email: "Software Engineer",
-				gender: "Edinburgh",
-				age: "23",
-				location: "2008/12/13",
-				phone_number: "$103",
-			},
-			{
-				username: "Ashton Cox",
-				email: "Junior Technical Author",
-				gender: "San Francisco",
-				age: "66",
-				location: "2009/01/12",
-				phone_number: "$86",
-			},
-			{
-				username: "Cedric Kelly",
-				email: "Senior Javascript Developer",
-				gender: "Edinburgh",
-				age: "22",
-				location: "2012/03/29",
-				phone_number: "$433",
-			},
-			{
-				username: "Airi Satou",
-				email: "Accountant",
-				gender: "Tokyo",
-				age: "33",
-				location: "2008/11/28",
-				phone_number: "$162",
-			},
-		],
-	});
+		},
+	];
 
-	// const getUserList = () => {
-	// 	Axios.
-	// }
+	const [users, setUsers] = useState(
+		new Array(37).fill(null).map((data, index) => {
+			const key = index + 1;
+			return {
+				username: `username${key}`,
+				email: `username${key}@email.com`,
+				age: `${key * 20}`,
+				gender: "male",
+				location: "Jakarta",
+				phoneNumber: "081234567890",
+			};
+		})
+	);
+
+	const options = {
+		sizePerPageList: [
+			{
+				text: "5",
+				value: 5,
+			},
+			{
+				text: "10",
+				value: 10,
+			},
+		],
+	};
 
 	return (
-		<MDBDataTableV5
-			hover
-			searching={false}
-			data={datatable}
-			entries={5}
-			entriesOptions={[5, 10, 15]}
+		<BootstrapTable
+			keyField="id"
+			data={users}
+			columns={columns}
+			pagination={paginationFactory(options)}
 		/>
 	);
 }
