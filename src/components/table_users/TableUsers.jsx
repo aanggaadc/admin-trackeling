318cd5603bdd6d1d599b7f23c810a47bfb926a41
deriscode <jdrepositories@gmail.com>
2022-06-12 21:03:57 +0700

    setting koneksi API

--- src/components/table_users/TableUsers.jsx
+++ src/components/table_users/TableUsers.jsx
@@ -5,73 +5,104 @@ import BootstrapTable from "react-bootstrap-table-next";
 import paginationFactory from "react-bootstrap-table2-paginator";
 import Axios from "axios";
 import "./TableUsers.scss";
+import { Button } from "react-bootstrap";
+import { FaEdit, FaTrash } from "react-icons/fa";
+import { API_URL } from "../../config/url";
 
 function TableUsers() {
 	const columns = [
 		{
+			dataField: "no",
+			text: "No",
+			headerStyle: () => {
+				return { width: "22%" };
+			},
+		},
+		{
 			dataField: "username",
 			text: "Username",
 			headerStyle: () => {
-				return { width: "35%" };
+				return { width: "50%" };
 			},
 		},
 		{
 			dataField: "email",
 			text: "Email",
 			headerStyle: () => {
-				return { width: "40%" };
+				return { width: "50%" };
 			},
 		},
 		{
 			dataField: "age",
 			text: "Age",
 			headerStyle: () => {
-				return { width: "20%" };
+				return { width: "25%" };
 			},
 		},
 		{
 			dataField: "gender",
 			text: "Gender",
 			headerStyle: () => {
-				return { width: "30%" };
+				return { width: "32%" };
 			},
 		},
 		{
 			dataField: "location",
 			text: "Location",
 			headerStyle: () => {
-				return { width: "30%" };
+				return { width: "49%" };
 			},
 		},
 		{
 			dataField: "phoneNumber",
 			text: "Phone Number",
 			headerStyle: () => {
-				return { width: "35%" };
+				return { width: "47%" };
+			},
+		},
+		{
+			dataField: "phoneNumber",
+			text: "Phone Number",
+			headerStyle: () => {
+				return { width: "47%" };
+			},
+		},
+		{
+			dataField: "isActive",
+			text: "is Active",
+			headerStyle: () => {
+				return { width: "47%" };
+			},
+		},
+		{
+			dataField: "",
+			text: "Actions",
+			headerStyle: () => {
+				return { width: "45%" };
+			},
+			formatter: (cell, row) => {
+				return (
+					<div className="text-center">
+						<Button variant="none">
+							<FaEdit color="blue" />
+						</Button>
+						<Button variant="none">
+							<FaTrash color="red" />
+						</Button>
+					</div>
+				);
 			},
 		},
 	];
 
-	const [users, setUsers] = useState(
-		new Array(37).fill(null).map((data, index) => {
-			const key = index + 1;
-			return {
-				username: `username${key}`,
-				email: `username${key}@email.com`,
-				age: `${key * 20}`,
-				gender: "male",
-				location: "Jakarta",
-				phoneNumber: "081234567890",
-			};
-		})
-	);
+	const [users, setUsers] = useState([]);
+
+	// const getUserList = () => {
+	// 	Axios.get(`${API_URL / user / all}`);
+	// };
 
 	const options = {
 		sizePerPageList: [
-			{
-				text: "5",
-				value: 5,
-			},
 			{
 				text: "10",
 				value: 10,
@@ -79,6 +110,10 @@ function TableUsers() {
 			{
 				text: "15",
 				value: 15,
+			},
+			{
+				text: "20",
+				value: 20,
 			},
 		],
 	};
