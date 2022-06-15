1960bb9ddfc0c0a6ad5618865c7a624f030f0cdb
deriscode <jdrepositories@gmail.com>
2022-06-15 16:41:39 +0700

    styling table user, table trip, & logout button

--- src/pages/trips/Trips.jsx
+++ src/pages/trips/Trips.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
+import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
-import TripList from '../../components/TripList/TripList';
-import './Trips.scss'
-import FilterList from '../../components/FilterList/FilterList';
+import TripList from "../../components/TripList/TripList";
+import "./Trips.scss";
+import FilterList from "../../components/FilterList/FilterList";
 import { useState } from "react";
 
 function Trips() {
-  const [filter, setFilter] = useState({
-    trip_name: "",
-    destination: "",
-    start_date: "",
-    end_date: "",
-    count_member: "",
-    max_member: ""
-    })
-  return (
-    <div className="trips">
-        <Sidebar />
-        <div className="tripsContainer">
-            <Navbar />
-            <FilterList filter={filter} setFilter={setFilter} />
-            <TripList filter={filter} setFilter={setFilter} />
-        </div> 
-    </div>
-  )
+	const [filter, setFilter] = useState({
+		trip_name: "",
+		destination: "",
+		start_date: "",
+		end_date: "",
+		count_member: "",
+		max_member: "",
+	});
+	return (
+		<div className="trips">
+			<Sidebar />
+			<div className="tripsContainer">
+				<Navbar />
+				<div className="contentContainer">
+					<div className="title">TRIP LIST</div>
+					<FilterList filter={filter} setFilter={setFilter} />
+					<TripList filter={filter} setFilter={setFilter} />
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Trips
+export default Trips;
