5c1d060e58e93ef1f962628abab9e5308860b51d
deriscode <jdrepositories@gmail.com>
2022-06-16 13:57:40 +0700

    membuat fungsi logout admin

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { CLIENT_URL } from "../../config/url";
+import { toast } from "react-toastify";
 import "./navbar.scss";
 
 function Navbar() {
+	const handleLogout = () => {
+		toast.success("You are logged out");
+		localStorage.removeItem("adminKey");
+	};
+
 	return (
 		<div>
 			<div className="navbar-custom">
 				<div className="wrapper">
 					<div className="items">
 						<div className="item">admin</div>
-						<div className="item item-logout">Logout</div>
+						<div className="item">
+							<a href={`${CLIENT_URL}/login-admin`} className="item-logout" onClick={handleLogout}>
+								Logout
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
