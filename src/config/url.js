b8b84a449999a21658360648231173275e40f51f
deriscode <jdrepositories@gmail.com>
2022-06-16 17:07:53 +0700

    change api & client url

--- src/config/url.js
+++ src/config/url.js
@@ -1,2 +1,5 @@
-export const API_URL = process.env.REACT_APP_API_URL
-export const CLIENT_URL = process.env.REACT_APP_CLIENT_URL
+// export const API_URL = process.env.REACT_APP_API_URL
+// export const CLIENT_URL = process.env.REACT_APP_CLIENT_URL
+
+export const API_URL = "https://api.trackeling.xyz";
+export const CLIENT_URL = "https://trackeling.xyz";
